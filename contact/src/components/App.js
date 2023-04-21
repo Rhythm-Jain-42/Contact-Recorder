882b8c1f7dcac367c05a830a4c756f9e75c8809b
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-04-21 21:21:07 +0530

    next commit

--- contact/src/components/App.js
+++ contact/src/components/App.js
@@ -1,23 +1,29 @@
-import React, {useState} from 'react'
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-const [contacts, setContacts] = useState([]);
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
 
-const addContactHandler=(contact) =>{
-  console.log(contact);
-  setContacts(...contacts, contact);
-}
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div className='ui container'>
+    <div className="ui container">
       <Header />
-      <AddContact addContactHandler={addContactHandler}/>
-      <ContactList contacts={contacts}/>
+      <AddContact addContactHandler={addContactHandler} />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
 
-export default App
+export default App;
