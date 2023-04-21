8c4cba216d7262c2840e4f4b7b18d70f1dc69100
Rhythm-Jain-42 <rhythmjain101@gmail.com>
2023-04-22 03:57:58 +0530

    next commit

--- contact/src/components/ContactCard.js
+++ contact/src/components/ContactCard.js
@@ -13,6 +13,7 @@ const ContactCard = (props) => {
       <i
         className="trash alternate outline icon"
         style={{ color: "red", marginTop: "7px" }}
+        onClick={() => props.clickHandler(id)}
       ></i>
     </div>
   );
--- contact/src/components/ContactList.js
+++ contact/src/components/ContactList.js
@@ -4,8 +4,11 @@ import ContactCard from "./ContactCard";
 const ContactList = (props) => {
   console.log(props);
 
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
   const renderContactList = props.contacts.map((contact) => {
-    return <ContactCard contact={contact}></ContactCard>;
+    return <ContactCard contact={contact} clickHandler = {deleteContactHandler}></ContactCard>;
   });
 
   return <div className="ui celled list">{renderContactList}</div>;
